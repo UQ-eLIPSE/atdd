24fc417d07fbeb6781eccce28b93b47207d388c0
Jo-Zh <joannaweinazh@gmail.com>
2023-11-08 11:53:25 +1000

    update test

--- e2e/infospot_test.cy.ts
+++ e2e/infospot_test.cy.ts
@@ -1,15 +1,9 @@
-describe("Minimap Test", () => {
+describe("Test case: InforLinknode in Expanded Left Top Bar should match linkNode in 3D view", () => {
   it("should compare elements with .hotspot-tooltip and .linkNodeNames", () => {
     // Visit the page where your divs are located
     cy.visit("/");
-    // Click a html element with attribute data-cy="minimap-small"
-    cy.get('[data-cy="sb-site"]').click();
-    cy.wait(500);
-    // cy.get('[data-cy="minimap-small"]').should("exist").click();
-    // Click the first element with data-cy="node"
-    // cy.get("#34-img_4073-panorama").first().should("exist").click();
 
-    // Retrieve the collection 1 of elements with .hotspot-tooltip that are visible
+    cy.get('[data-cy="sb-site"]').click();
     cy.wait(1000);
     cy.get(".hotspot.link-hotspot")
       .filter((index, element) => {
